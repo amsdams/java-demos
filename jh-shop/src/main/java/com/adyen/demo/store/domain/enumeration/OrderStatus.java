d32b0a4a09e349b712143dd23bac08722b6d6507
Julien Langeveld <julien.langeveld@kramphub.com>
2021-11-11 11:58:31 +0100

    Initial version of store generated by generator-jhipster@7.1.0

---
+++ jh-shop/src/main/java/com/adyen/demo/store/domain/enumeration/OrderStatus.java
@@ -0,0 +1,11 @@
+package com.adyen.demo.store.domain.enumeration;
+
+/**
+ * The OrderStatus enumeration.
+ */
+public enum OrderStatus {
+    COMPLETED,
+    PAID,
+    PENDING,
+    CANCELLED,
+}
---
+++ jh-shop/src/main/java/com/adyen/demo/store/domain/enumeration/PaymentMethod.java
@@ -0,0 +1,9 @@
+package com.adyen.demo.store.domain.enumeration;
+
+/**
+ * The PaymentMethod enumeration.
+ */
+public enum PaymentMethod {
+    CREDIT_CARD,
+    IDEAL,
+}
